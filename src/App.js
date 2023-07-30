eb2e4ffe2a83e2700277511cabb5f791a14c3ce5
Don Lee <donlee-git@anonmails.anonaddy.me>
2023-07-30 19:17:18 +0100

    Creates a function for search results.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// f5c10180
+
+const API_URL = 'http://omdbapi.com?apikey=f5c10180';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []);
+
     return (
         <h1>App</h1>
     );
